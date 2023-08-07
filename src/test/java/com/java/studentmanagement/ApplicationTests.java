f3760fdd03ae40bdce5c90f80c85d17c8176e053
Manjunath.Vallu <manjunath.vallu@target.com>
2023-08-07 23:40:28 +0530

    added tests for student controller and application

--- src/test/java/com/java/studentmanagement/ApplicationTests.java
+++ src/test/java/com/java/studentmanagement/ApplicationTests.java
@@ -1,14 +1,23 @@
 package com.java.studentmanagement;
 
+import com.java.studentmanagement.controller.StudentController;
 import com.java.studentmanagement.service.StudentService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes=StudentService.class)
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
 class ApplicationTests {
+	@Autowired
+	private StudentController studentController;
+	@Autowired
+	private StudentService studentService;
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		assertThat(studentController).isNotNull();
+		assertThat(studentService).isNotNull();
 	}
-
 }
---
+++ src/test/java/com/java/studentmanagement/service/StudentServiceImplTest.java
@@ -0,0 +1,9 @@
+package com.java.studentmanagement.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class StudentServiceImplTest {
+//    TODO: Add student service test here
+}
